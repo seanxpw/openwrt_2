b83f376c73bf1de36a0adc56a9434310e4ab0874
seanxpw <89694133+seanxpw@users.noreply.github.com>
2022-10-07 16:41:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git ./package/
+git clone https://github.com/kenzok8/small.git package/luci-app-passwall/small
